b5ac9329cd0046fd0139761496e51b7fc844dcd4
Giuseppe Barresi <peppe.barresi1@gmail.com>
2023-10-11 16:16:33 +0200

    firstcommit

---
+++ js.js
@@ -0,0 +1,22 @@
+const task = document.getElementById("Task");
+const myList = document.getElementById("my-list");
+const button = document.getElementById("my-button");
+
+button.onclick = function () {
+  let elementList = document.createElement("li");
+  let newbutton = document.createElement("button");
+
+  elementList.innerText = task.value;
+  if (task.value !== "") {
+    myList.appendChild(elementList);
+    task.value = "";
+    elementList.addEventListener("click", function (e) {
+      console.log(e.target);
+      e.target.classList.add("linethrough");
+      elementList.appendChild(newbutton);
+      newbutton.addEventListener("click", function (e) {
+        e.target.parentElement.remove();
+      });
+    });
+  }
+};
