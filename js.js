6fa4b02f0e78d4de0ab1fc8a25e6116ffaf5380d
Giuseppe Barresi <peppe.barresi1@gmail.com>
2023-10-11 17:00:05 +0200

    2nd

--- js.js
+++ js.js
@@ -1,22 +1,24 @@
-const task = document.getElementById("Task");
-const myList = document.getElementById("my-list");
-const button = document.getElementById("my-button");
+window.onload = () => {
+  const task = document.getElementById("Task");
+  const myList = document.getElementById("my-list");
+  const button = document.getElementById("my-button");
 
-button.onclick = function () {
-  let elementList = document.createElement("li");
-  let newbutton = document.createElement("button");
+  button.onclick = function () {
+    let elementList = document.createElement("li");
+    let newbutton = document.createElement("button");
 
-  elementList.innerText = task.value;
-  if (task.value !== "") {
-    myList.appendChild(elementList);
-    task.value = "";
-    elementList.addEventListener("click", function (e) {
-      console.log(e.target);
-      e.target.classList.add("linethrough");
-      elementList.appendChild(newbutton);
-      newbutton.addEventListener("click", function (e) {
-        e.target.parentElement.remove();
+    elementList.innerText = task.value;
+    if (task.value !== "") {
+      myList.appendChild(elementList);
+      task.value = "";
+      elementList.addEventListener("click", function (e) {
+        console.log(e.target);
+        e.target.classList.add("linethrough");
+        elementList.appendChild(newbutton);
+        newbutton.addEventListener("click", function (e) {
+          e.target.parentElement.remove();
+        });
       });
-    });
-  }
+    }
+  };
 };
